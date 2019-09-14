c03773b3a4991180101e047d45181640691bc73a
Andreu Gallofré <andreu.gallofre@capside.com>
2019-09-14 18:46:48 +0200

    Add API interaction for the login page

--- frontend_orgs/src/App.js
+++ frontend_orgs/src/App.js
@@ -1,5 +1,5 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/Login/Login.js'
 
--- frontend_orgs/src/api/api.js
+++ frontend_orgs/src/api/api.js
@@ -1,5 +1,9 @@
-const baseURL = "";
+import axios from "axios";
+import Cookies from "universal-cookie";
+
 const cookies = new Cookies();
+
+const baseURL = "http://htn.oriolclosa.dev/api/v1.0";
 
 const api = axios.create({
     baseURL: baseURL
@@ -13,23 +17,50 @@ if (cookies.get("sid") === undefined ) {
     api.logged_in = true;
 }
 
+api.logged_in = false;
+
+let sid = cookies.get("sid");
+if (sid) setSid(sid);
+
+function setUsername(username) {
+    cookies.set("username", username, { path: "/" });
+}
+
+function setEmail(email) {
+    cookies.set("email", email, { path: "/" });
+}
+
 function setSid(sid) {
-    cookies.set("sid", sid);
+    cookies.set("sid", sid, { path: "/" });
     api.defaults.headers.post["Authorization"] = sid;
     api.defaults.headers.get["Authorization"] = sid;
     api.defaults.headers.put["Authorization"] = sid;
     api.defaults.headers.delete["Authorization"] = sid;
     api.logged_in = sid !== "null" && typeof sid !== "undefined";
-    console.log(api.logged_in);
 }
 
-function getCookie() {
-    return cookies.get("sid");
+function setAccess(access) {
+    cookies.set("access", access, {path: "/"});
+}
+
+function getAccess() {
+    return cookies.get("access");
+}
+
+function getUsername() {
+    return cookies.get("username");
+}
+
+function getEmail() {
+    return cookies.get("email");
 }
 
 function logout() {
     setSid("null");
+    setUsername("");
+    setEmail("");
+    setAccess("");
 }
 
 export default api;
-export { setSid, logout, getCookie };
+export { setSid, setUsername, getUsername, logout, setEmail, getEmail, setAccess, getAccess };
