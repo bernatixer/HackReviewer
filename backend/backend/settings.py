8f9b5d359523461061121f741c236d67e908b65b
Oriol Closa <oriol.closa@est.fib.upc.edu>
2019-09-14 17:40:28 -0400

    Add backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
     'knox',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
